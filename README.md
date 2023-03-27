c4b6291839ef5b2619a870c202e9b5feb33fe482
Bibek Aryal <bibekaryal86@gmail.com>
2023-03-27 12:10:28 -0700

    Update Monitor Threads

--- README.md
+++ README.md
@@ -1,14 +1,15 @@
 # dependencies-updater
 
 * Things to update:
-  * Add scheduled tasks
-    * (DONE) Task 1, check mongo for updated list of gradle plugins (once a week?)
-    * Task 2, check mongo and maven for any updates required in mongo (once a week?)
-  * Update log configurations
-    * only warn/error on stdout
-      * update ThreadMonitor.java log level
-    * everything on file log
-  * Rename `execute` folder to `helpers`
+    * Add scheduled tasks
+        * (DONE) Task 1, check mongo for updated list of gradle plugins (once a week?)
+        * Task 2, check mongo and maven for any updates required in mongo (once a week?)
+    * Update log configurations
+        * only warn/error on stdout
+            * update ThreadMonitor.java log level
+        * everything on file log
+    * https://www.logicbig.com/how-to/code-snippets/jcode-java-command-line-animation
 
 * Example:
-  * java -jar -Drepo_home=/home/pi/zava/projects -Dmongo_user=something_user -Dmongo_pwd=something_password app-dependency-update.jar
+    * java -jar -Drepo_home=/home/pi/zava/projects -Dmongo_user=something_user -Dmongo_pwd=something_password
+      app-dependency-update.jar
--- app/src/main/java/app/dependency/update/app/execute/ThreadMonitor.java
+++ app/src/main/java/app/dependency/update/app/execute/ThreadMonitor.java
@@ -3,6 +3,7 @@ package app.dependency.update.app.execute;
 import app.dependency.update.app.model.AppInitData;
 import app.dependency.update.app.model.Repository;
 import app.dependency.update.app.util.CommonUtil;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -22,11 +23,18 @@ public class ThreadMonitor {
   }
 
   private void monitorThreads() {
-    Set<Thread> threads = Thread.getAllStackTraces().keySet();
-    monitorNpmThreads(threads);
+    final Set<Thread> threads = Thread.getAllStackTraces().keySet();
+    Map<String, Thread.State> threadStatusMap = new HashMap<>();
+    threadStatusMap.putAll(monitorNpmThreads(threads));
+    threadStatusMap.putAll(monitorGradleWrapperThreads(threads));
+    threadStatusMap.putAll(monitorGradleDependenciesThreads(threads));
+
+    if (threadStatusMap.size() > 0) {
+      log.warn("Current Threads Executing Updates: {}", threadStatusMap);
+    }
   }
 
-  private void monitorNpmThreads(final Set<Thread> threads) {
+  private Map<String, Thread.State> monitorNpmThreads(final Set<Thread> threads) {
     // thread names are set as repository names
     List<String> npmThreadNames =
         this.appInitData.getRepositories().stream()
@@ -39,12 +47,39 @@ public class ThreadMonitor {
         threads.stream().filter(thread -> npmThreadNames.contains(thread.getName())).toList();
 
     // npm threads with status
-    Map<String, Thread.State> threadStatusMap =
-        npmThreads.stream().collect(Collectors.toMap(Thread::getName, Thread::getState));
+    return npmThreads.stream().collect(Collectors.toMap(Thread::getName, Thread::getState));
+  }
 
-    // print threads and their status
-    if (threadStatusMap.size() > 0) {
-      log.info("Current NPM threads: {}", threadStatusMap);
-    }
+  private Map<String, Thread.State> monitorGradleWrapperThreads(final Set<Thread> threads) {
+    List<String> gradleWrapperThreadNames =
+        this.appInitData.getRepositories().stream()
+            .filter(repository -> repository.getType().equals(CommonUtil.GRADLE))
+            .map(repository -> repository.getRepoName() + "_" + CommonUtil.WRAPPER)
+            .toList();
+
+    // gradle wrapper threads
+    List<Thread> gradleWrapperThreads =
+        threads.stream()
+            .filter(thread -> gradleWrapperThreadNames.contains(thread.getName()))
+            .toList();
+
+    // gradle wrapper threads with status
+    return gradleWrapperThreads.stream()
+        .collect(Collectors.toMap(Thread::getName, Thread::getState));
+  }
+
+  private Map<String, Thread.State> monitorGradleDependenciesThreads(final Set<Thread> threads) {
+    List<String> gradleThreadNames =
+        this.appInitData.getRepositories().stream()
+            .filter(repository -> repository.getType().equals(CommonUtil.GRADLE))
+            .map(Repository::getRepoName)
+            .toList();
+
+    // gradle threads
+    List<Thread> gradleThreads =
+        threads.stream().filter(thread -> gradleThreadNames.contains(thread.getName())).toList();
+
+    // gradle threads with status
+    return gradleThreads.stream().collect(Collectors.toMap(Thread::getName, Thread::getState));
   }
 }
