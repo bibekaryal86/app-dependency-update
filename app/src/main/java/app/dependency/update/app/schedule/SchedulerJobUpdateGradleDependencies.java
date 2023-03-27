b26f2be92d3ab2a368a8572a0149ab4b37bfc818
Bibek Aryal <bibekaryal86@gmail.com>
2023-03-27 09:18:46 -0700

    Add Scheduler to Update NPM Dependencies and Gradle Wrapper

--- app/src/main/java/app/dependency/update/app/schedule/SchedulerJobUpdateGradleDependencies.java
+++ app/src/main/java/app/dependency/update/app/schedule/SchedulerJobUpdateGradleDependencies.java
@@ -1,9 +1,9 @@
 package app.dependency.update.app.schedule;
 
-import lombok.extern.slf4j.Slf4j;
+import app.dependency.update.app.model.AppInitData;import app.dependency.update.app.util.CommonUtil;import lombok.extern.slf4j.Slf4j;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
-import org.quartz.JobExecutionException;
+import org.quartz.JobExecutionException;import java.util.concurrent.CompletableFuture;
 
 @Slf4j
 public class SchedulerJobUpdateGradleDependencies implements Job {
@@ -11,7 +11,16 @@ public class SchedulerJobUpdateGradleDependencies implements Job {
   @Override
   public void execute(JobExecutionContext context) throws JobExecutionException {
     log.info("Start SchedulerJobUpdateGradleDependencies...");
+    final AppInitData appInitData =
+            (AppInitData) context.getJobDetail().getJobDataMap().get(CommonUtil.APP_INIT_DATA_MAP);
+    CompletableFuture.runAsync(() -> updateGradleDependencies(appInitData));
+  }
+
+  private void updateGradleDependencies(final AppInitData appInitData) {
+
     // TODO
     log.info("Finish SchedulerJobUpdateGradleDependencies...");
   }
+
+
 }
