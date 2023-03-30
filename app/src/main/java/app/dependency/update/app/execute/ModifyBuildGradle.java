eb32b7c446bd1d8cd308e3e7b48f5fc20fcc6801
Bibek Aryal <bibekaryal86@gmail.com>
2023-03-30 15:12:49 -0700

    Update Definitions

--- app/src/main/java/app/dependency/update/app/execute/ModifyBuildGradle.java
+++ app/src/main/java/app/dependency/update/app/execute/ModifyBuildGradle.java
@@ -2,9 +2,11 @@ package app.dependency.update.app.execute;
 
 import app.dependency.update.app.model.BuildGradleConfigs;
 import app.dependency.update.app.model.GradleConfigBlock;
+import app.dependency.update.app.model.GradleDefinition;
 import app.dependency.update.app.model.GradleDependency;
 import app.dependency.update.app.util.CommonUtil;
 import java.util.List;
+import java.util.Optional;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -21,39 +23,74 @@ public class ModifyBuildGradle {
     final List<String> originals = this.buildGradleConfigs.getOriginals();
 
     final GradleConfigBlock pluginsBlock = this.buildGradleConfigs.getPlugins();
-    updateDependencies(pluginsBlock, originals);
+    modifyConfigurations(pluginsBlock, originals);
 
     final GradleConfigBlock dependenciesBlock = this.buildGradleConfigs.getDependencies();
-    updateDependencies(dependenciesBlock, originals);
+    modifyConfigurations(dependenciesBlock, originals);
 
     log.info("AFTER CHANGE: [{}]", originals);
   }
 
-  private void updateDependencies(final GradleConfigBlock dependenciesBlock, final List<String> originals) {
+  private void modifyConfigurations(
+      final GradleConfigBlock dependenciesBlock, final List<String> originals) {
     if (dependenciesBlock != null && !dependenciesBlock.getDependencies().isEmpty()) {
-      for (final GradleDependency gradleDependency: dependenciesBlock.getDependencies()) {
-        String updatedOriginal = updateDependency(gradleDependency);
-        if (updatedOriginal != null) {
-          int index = originals.indexOf(gradleDependency.getOriginal());
-          if (index >= 0) {
-            originals.set(index, updatedOriginal);
+      for (final GradleDependency gradleDependency : dependenciesBlock.getDependencies()) {
+        if (gradleDependency.getVersion().startsWith("$")) {
+          // this updates definition
+          String defName = gradleDependency.getVersion().replace("$", "");
+          Optional<GradleDefinition> gradleDefinitionOptional =
+              dependenciesBlock.getDefinitions().stream()
+                  .filter(gradleDefinition -> gradleDefinition.getName().equals(defName))
+                  .findFirst();
+          if (gradleDefinitionOptional.isPresent()) {
+            GradleDefinition gradleDefinition = gradleDefinitionOptional.get();
+            String version = gradleDefinition.getValue();
+            GradleDependency modifiedGradleDependency =
+                GradleDependency.builder()
+                    .group(gradleDependency.getGroup())
+                    .artifact(gradleDependency.getArtifact())
+                    .version(version)
+                    .build();
+
+            String updatedOriginal =
+                modifyConfiguration(modifiedGradleDependency, gradleDefinition);
+
+            if (updatedOriginal != null) {
+              int index = originals.indexOf(gradleDefinition.getOriginal());
+              if (index >= 0) {
+                originals.set(index, updatedOriginal);
+              }
+            }
+          }
+        } else {
+          String updatedOriginal = modifyConfiguration(gradleDependency, null);
+          if (updatedOriginal != null) {
+            int index = originals.indexOf(gradleDependency.getOriginal());
+            if (index >= 0) {
+              originals.set(index, updatedOriginal);
+            }
           }
         }
       }
     }
   }
 
-  private String updateDependency(final GradleDependency gradleDependency) {
+  private String modifyConfiguration(
+      final GradleDependency gradleDependency, final GradleDefinition gradleDefinition) {
     String latestVersion =
         new MavenRepo()
             .getLatestVersion(
                 gradleDependency.getGroup(),
                 gradleDependency.getArtifact(),
-                gradleDependency.getVersion(),
+                gradleDefinition == null
+                    ? gradleDependency.getVersion()
+                    : gradleDefinition.getValue(),
                 false);
 
     if (CommonUtil.isRequiresUpdate(gradleDependency.getVersion(), latestVersion)) {
-      return gradleDependency.getOriginal().replace(gradleDependency.getVersion(), latestVersion);
+      return gradleDefinition == null
+          ? gradleDependency.getOriginal().replace(gradleDependency.getVersion(), latestVersion)
+          : gradleDefinition.getOriginal().replace(gradleDefinition.getValue(), latestVersion);
     }
 
     return null;
