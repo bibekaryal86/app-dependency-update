f2bd1d51d4585daf80b1b3b3b7e66d0d360f8e54
Bibek Aryal <bibekaryal86@gmail.com>
2023-03-30 13:22:58 -0700

    Modify build.gradle

--- app/src/main/java/app/dependency/update/app/execute/ModifyBuildGradle.java
+++ app/src/main/java/app/dependency/update/app/execute/ModifyBuildGradle.java
@@ -3,6 +3,8 @@ package app.dependency.update.app.execute;
 import app.dependency.update.app.model.BuildGradleConfigs;
 import app.dependency.update.app.model.GradleConfigBlock;
 import app.dependency.update.app.model.GradleDependency;
+import app.dependency.update.app.util.CommonUtil;
+import java.util.List;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -15,9 +17,36 @@ public class ModifyBuildGradle {
   }
 
   public void modifyBuildGradle() {
-    GradleConfigBlock pluginsBlock = this.buildGradleConfigs.getPlugins();
-    if (pluginsBlock != null && !pluginsBlock.getDependencies().isEmpty()) {}
+    log.info("BEFORE CHANGE: [{}]", this.buildGradleConfigs.getOriginals());
+    final List<String> originals = this.buildGradleConfigs.getOriginals();
+    final GradleConfigBlock pluginsBlock = this.buildGradleConfigs.getPlugins();
+
+    if (pluginsBlock != null && !pluginsBlock.getDependencies().isEmpty()) {
+      for (final GradleDependency gradleDependency : pluginsBlock.getDependencies()) {
+        String updatedOriginal = checkGradlePluginRequiresUpdate(gradleDependency);
+        if (updatedOriginal != null) {
+          int index = originals.indexOf(gradleDependency.getOriginal());
+          originals.set(index, updatedOriginal);
+        }
+      }
+    }
+
+    log.info("AFTER CHANGE: [{}]", originals);
   }
 
-  public void checkGradlePluginRequiresUpdate(final GradleDependency gradleDependency) {}
+  private String checkGradlePluginRequiresUpdate(final GradleDependency gradleDependency) {
+    String latestVersion =
+        new MavenRepo()
+            .getLatestVersion(
+                gradleDependency.getGroup(),
+                gradleDependency.getArtifact(),
+                gradleDependency.getVersion(),
+                false);
+
+    if (CommonUtil.isRequiresUpdate(gradleDependency.getVersion(), latestVersion)) {
+      return gradleDependency.getOriginal().replace(gradleDependency.getVersion(), latestVersion);
+    }
+
+    return null;
+  }
 }
