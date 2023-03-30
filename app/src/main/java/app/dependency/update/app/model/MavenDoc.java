613cf72d4b5974cfc17c26f3801be4432168b62b
Bibek Aryal <bibekaryal86@gmail.com>
2023-03-30 09:47:57 -0700

    Read build.gradle

--- app/src/main/java/app/dependency/update/app/model/MavenDoc.java
+++ app/src/main/java/app/dependency/update/app/model/MavenDoc.java
@@ -1,12 +1,10 @@
 package app.dependency.update.app.model;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.ToString;
 
 @Getter
-@Builder
 @ToString
 @AllArgsConstructor
 public class MavenDoc {
