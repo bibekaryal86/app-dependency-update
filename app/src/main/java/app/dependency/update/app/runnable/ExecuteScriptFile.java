359332409ec267bd680ef36b34b32d4a8114cf9c
Bibek Aryal <bibekaryal86@gmail.com>
2023-04-29 09:53:39 -0700

    Add Reset Endpoint

--- app/src/main/java/app/dependency/update/app/runnable/ExecuteScriptFile.java
+++ app/src/main/java/app/dependency/update/app/runnable/ExecuteScriptFile.java
@@ -71,8 +71,7 @@ public class ExecuteScriptFile implements Runnable {
   }
 
   private void processOutput(final Process process) throws AppDependencyUpdateIOException {
-    StringBuilder stringBuilder = new StringBuilder(this.threadName + "BEGIN\n");
-    stringBuilder.append(this.threadName).append("---BEGIN---").append("\n");
+    StringBuilder stringBuilder = new StringBuilder();
     String line;
 
     try (BufferedReader readerError =
@@ -88,7 +87,6 @@ public class ExecuteScriptFile implements Runnable {
         }
       }
 
-      stringBuilder.append(this.threadName).append("---END---");
       log.info("Process output: [ {} ]\n{}", this.scriptPath, stringBuilder);
     } catch (IOException ex) {
       throw new AppDependencyUpdateIOException(
