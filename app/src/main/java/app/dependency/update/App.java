efcac06df6e50a2e0cdd20c107ed377fcf8119b6
Bibek Aryal <bibekaryal86@gmail.com>
2023-05-01 12:09:20 -0700

    Fix Enable Async Annotation Missing

--- app/src/main/java/app/dependency/update/App.java
+++ app/src/main/java/app/dependency/update/App.java
@@ -12,14 +12,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
-import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @Slf4j
 @SpringBootApplication
 @EnableCaching
 @EnableScheduling
-@EnableAsync
 @EnableMongoRepositories
 public class App {
 
