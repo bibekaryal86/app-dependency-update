a12f4ffb109aeac6f0e1448647a28f9341ae8c0b
Bibek Aryal <bibekaryal86@gmail.com>
2023-09-09 07:45:08 -0700

    Modify Plugins Update

--- app/src/main/java/app/dependency/update/app/util/ApplicationContextUtil.java
+++ app/src/main/java/app/dependency/update/app/util/ApplicationContextUtil.java
@@ -7,15 +7,18 @@ import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Component;
 
 @Component
+/*
+  Utility class to access spring beans in static method/class
+ */
 public class ApplicationContextUtil implements ApplicationContextAware {
   private static ApplicationContext context;
 
   // suppressing sonarlint rule for static field maintainability
   @SuppressWarnings("java:S2696")
   @Override
-  public void setApplicationContext(@NotNull ApplicationContext argApplicationContext)
+  public void setApplicationContext(@NotNull ApplicationContext applicationContext)
       throws BeansException {
-    context = argApplicationContext;
+    context = applicationContext;
   }
 
   public static ApplicationContext getApplicationContext() {
