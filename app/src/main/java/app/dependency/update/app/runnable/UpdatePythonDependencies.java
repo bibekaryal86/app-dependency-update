c21f246f9481addd94fe71e3af6a458c0fbc5835
Bibek Aryal <bibekaryal86@gmail.com>
2023-09-15 14:58:36 -0700

    Add Update Dependencies to Scheduler

---
+++ app/src/main/java/app/dependency/update/app/runnable/UpdatePythonDependencies.java
@@ -0,0 +1,67 @@
+package app.dependency.update.app.runnable;
+
+import static app.dependency.update.app.util.CommonUtils.*;
+import static app.dependency.update.app.util.ConstantUtils.*;
+
+import app.dependency.update.app.exception.AppDependencyUpdateRuntimeException;
+import app.dependency.update.app.model.AppInitData;
+import app.dependency.update.app.model.Repository;
+import app.dependency.update.app.model.ScriptFile;
+import app.dependency.update.app.service.MavenRepoService;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class UpdatePythonDependencies {
+  private final List<Repository> repositories;
+  private final ScriptFile scriptFile;
+  private final MavenRepoService mavenRepoService;
+
+  public UpdatePythonDependencies(
+      final AppInitData appInitData, final MavenRepoService mavenRepoService) {
+    this.repositories =
+        appInitData.getRepositories().stream()
+            .filter(repository -> repository.getType().equals(UpdateType.PYTHON_DEPENDENCIES))
+            .toList();
+    this.scriptFile =
+        appInitData.getScriptFiles().stream()
+            .filter(sf -> sf.getType().equals(UpdateType.PYTHON_DEPENDENCIES))
+            .findFirst()
+            .orElseThrow(
+                () ->
+                    new AppDependencyUpdateRuntimeException(
+                        "Python Dependencies Script Not Found..."));
+    this.mavenRepoService = mavenRepoService;
+  }
+
+  public void updatePythonDependencies() {
+    List<Thread> threads = new ArrayList<>();
+    for (Repository repository : this.repositories) {
+      threads.add(executeUpdate(repository));
+    }
+    threads.forEach(this::join);
+  }
+
+  private Thread executeUpdate(final Repository repository) {
+    log.debug("Execute Python Dependencies Update on: [ {} ]", repository);
+    List<String> arguments = new LinkedList<>();
+    arguments.add(repository.getRepoPath().toString());
+    arguments.add(String.format(BRANCH_UPDATE_DEPENDENCIES, LocalDate.now()));
+
+    return new ExecutePythonUpdate(repository, this.scriptFile, arguments, mavenRepoService)
+        .start();
+  }
+
+  // suppressing sonarlint rule for interrupting thread
+  @SuppressWarnings("java:S2142")
+  private void join(final Thread thread) {
+    try {
+      thread.join();
+    } catch (InterruptedException ex) {
+      log.error("Exception Join Thread", ex);
+    }
+  }
+}
