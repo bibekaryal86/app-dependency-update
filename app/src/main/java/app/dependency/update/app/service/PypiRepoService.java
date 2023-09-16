6d5790248595b774df0d8fa005f7e6005af1b8a2
Bibek Aryal <bibekaryal86@gmail.com>
2023-09-15 19:58:47 -0700

    Update Requirements Txt

--- app/src/main/java/app/dependency/update/app/service/PypiRepoService.java
+++ app/src/main/java/app/dependency/update/app/service/PypiRepoService.java
@@ -2,10 +2,8 @@ package app.dependency.update.app.service;
 
 import app.dependency.update.app.connector.PypiConnector;
 import app.dependency.update.app.model.PypiSearchResponse;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
-@Slf4j
 @Service
 public class PypiRepoService {
 
