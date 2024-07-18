0782cf485292b266ff3bfe16acef07b84a02f29a
Bibek Aryal <bibekaryal86@gmail.com>
2024-07-17 23:16:53 -0600

    Process Summary Email

--- app/src/main/java/app/dependency/update/app/service/UpdateRepoService.java
+++ app/src/main/java/app/dependency/update/app/service/UpdateRepoService.java
@@ -330,30 +330,132 @@ public class UpdateRepoService {
   }
 
   private String getProcessSummaryContent() {
-    ProcessSummary processSummary = getProcessSummary();
-
-    return null;
-  }
-
-  private ProcessSummary getProcessSummary() {
     List<ProcessedRepository> processedRepositories =
         ProcessUtils.getProcessedRepositoriesMap().values().stream()
             .sorted(Comparator.comparing(ProcessedRepository::getRepoName))
             .toList();
 
-    return ProcessSummary.builder()
-        .mongoPluginsToUpdate(ProcessUtils.getMongoPluginsToUpdate())
-        .mongoDependenciesToUpdate(ProcessUtils.getMongoDependenciesToUpdate())
-        .mongoPackagesToUpdate(ProcessUtils.getMongoPackagesToUpdate())
-        .mongoNpmSkipsActive(ProcessUtils.getMongoNpmSkipsActive())
-        .totalPrCreatedCount(
-            (int) processedRepositories.stream().filter(ProcessedRepository::isPrCreated).count())
-        .totalPrCreateErrorsCount(
-            (int)
-                processedRepositories.stream().filter(ProcessedRepository::isPrCreateError).count())
-        .totalPrMergedCount(
-            (int) processedRepositories.stream().filter(ProcessedRepository::isPrMerged).count())
-        .processedRepositories(processedRepositories)
-        .build();
+    ProcessSummary processSummary =
+        ProcessSummary.builder()
+            .mongoPluginsToUpdate(ProcessUtils.getMongoPluginsToUpdate())
+            .mongoDependenciesToUpdate(ProcessUtils.getMongoDependenciesToUpdate())
+            .mongoPackagesToUpdate(ProcessUtils.getMongoPackagesToUpdate())
+            .mongoNpmSkipsActive(ProcessUtils.getMongoNpmSkipsActive())
+            .totalPrCreatedCount(
+                (int)
+                    processedRepositories.stream().filter(ProcessedRepository::isPrCreated).count())
+            .totalPrCreateErrorsCount(
+                (int)
+                    processedRepositories.stream()
+                        .filter(ProcessedRepository::isPrCreateError)
+                        .count())
+            .totalPrMergedCount(
+                (int)
+                    processedRepositories.stream().filter(ProcessedRepository::isPrMerged).count())
+            .processedRepositories(processedRepositories)
+            .build();
+
+    return getProcessSummaryContent(processSummary);
+  }
+
+  private String getProcessSummaryContent(ProcessSummary processSummary) {
+    StringBuilder html = new StringBuilder();
+    html.append(
+        """
+            <html>
+              <head>
+                <style>
+                  th {
+                      border-bottom: 2px solid #9e9e9e;
+                      position: sticky;
+                      top: 0;
+                      background-color: lightgrey;
+                    }
+                  td {
+                    padding: 5px;
+                    text-align: left;
+                    border-bottom: 1px solid #9e9e9e;
+                  }
+                </style>
+              </head>
+              <body>
+            """);
+
+    html.append(
+        """
+          <p style='font-size: 14px; font-weight: bold;'>App Dependency Update Process Summary</p>
+          <table cellpadding='10' cellspacing='0' style='font-size: 12px; border-collapse: collapse;'>
+            <tr>
+              <th>Item</th>
+              <th>Value</th>
+            </tr>
+            <tr>
+              <td>Mongo Plugins To Update</td>
+              <td>%d</td>
+            </tr>
+            <tr>
+              <td>Mongo Dependencies To Update</td>
+              <td>%d</td>
+            </tr>
+            <tr>
+              <td>Mongo Packages To Update</td>
+              <td>%d</td>
+            </tr>
+            <tr>
+              <td>Mongo NPM Skips Active</td>
+              <td>%d</td>
+            </tr>
+            <tr>
+              <td>Total PR Created Count</td>
+              <td>%d</td>
+            </tr>
+            <tr>
+              <td>Total PR Create Errors Count</td>
+              <td>%d</td>
+            </tr>
+            <tr>
+              <td>Total PR Merged Count</td>
+              <td>%d</td>
+            </tr>
+          </table>
+        """
+            .formatted(
+                processSummary.getMongoPluginsToUpdate(),
+                processSummary.getMongoDependenciesToUpdate(),
+                processSummary.getMongoPackagesToUpdate(),
+                processSummary.getMongoNpmSkipsActive(),
+                processSummary.getTotalPrCreatedCount(),
+                processSummary.getTotalPrCreateErrorsCount(),
+                processSummary.getTotalPrMergedCount()));
+
+    html.append(
+        """
+          <br />
+          <p style='font-size: 14px; font-weight: bold;'>Processed Repositories</p>
+          <table border='1' cellpadding='10' cellspacing='0' style='border-collapse: collapse; width: 100%;'>
+            <tr>
+              <th>Repository</th>
+              <th>PR Created</th>
+              <th>PR Create Error</th>
+              <th>PR Merged</th>
+            </tr>
+        """);
+
+    for (ProcessedRepository processedRepository : processSummary.getProcessedRepositories()) {
+      html.append("<tr>");
+      html.append("<td>").append(processedRepository.getRepoName()).append("</td>");
+      html.append("<td>").append(processedRepository.isPrCreated() ? "Y" : "N").append("</td>");
+      html.append("<td>").append(processedRepository.isPrCreateError() ? "Y" : "N").append("</td>");
+      html.append("<td>").append(processedRepository.isPrMerged() ? "Y" : "N").append("</td>");
+      html.append("</tr>");
+    }
+
+    html.append("""
+          </table>
+          </body>
+        </html>
+        """);
+
+    return html.toString();
   }
 }
